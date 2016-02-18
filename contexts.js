b9c756d2cd8f3ad1d2d0cb24a0883b9508a44668
Fabio Masini <fabio@rapidosoft.it>
2016-02-18 16:39:09 +0100

    base html must be included as node module

--- contexts.js
+++ contexts.js
@@ -42,16 +42,10 @@ class RuntimeContext {
     if (this.isClient) {
       this._renderContainerObject = document.getElementById(this.containerNodeId);
     } else {
-      var fs = require('fs');
-      fs.readFile('./dist/index.html', (err, html) => {
-        if (err) {
-          throw err;
-        }
-        //make urls absolute
-        html = `${html}`;
-        html = html.replace(/"((?:[^"]*?)\.(?:js|css))"/g, '"/$1"')
-        this._renderContainerObject = html;
-      });
+      // the project bundler *MUST* include a __main.html module with the base html source
+      var html = require('__main.html');
+      html = html.replace(/"((?:[^"]*?)\.(?:js|css))"/g, '"/$1"')
+      this._renderContainerObject = html;
     }
     for (var mid of settings.MIDDLEWARE) {
       this._middleware.push(new mid.default()); //eslint-disable-line new-cap
