f5b9ceb225a5dc308beb9d98585d83baf70976dd
Fabio Masini <fabio@rapidosoft.it>
2016-02-25 11:16:11 +0100

    JSDoc!

--- boot.js
+++ boot.js
@@ -1,9 +1,22 @@
+/**
+ * Bootstrap utils module.
+ * @module outlinejs/boot
+ */
 import 'babel-polyfill';
 import { _initContexts } from './contexts';
 import './utils/patches/backbone';
 import { RouteUtils } from './routers';
 
+/**
+ * Class for bootstrapping an outlineJS project.
+ */
 export default class {
+  /**
+   * Bootstrap the outlineJS isomorphic application.
+   * @param {BaseSettings} settingsClass - The project settings class
+   * @param {BaseRouter} routerClass - The main router class defined in main urls.js module
+   * @param {string} containerNodeId - The node id where to render the view layer
+   */
   static init(settingsClass, routerClass, containerNodeId) {
     _initContexts(settingsClass, containerNodeId);
     RouteUtils.listen(routerClass);
